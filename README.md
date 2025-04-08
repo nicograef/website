57f7be8e345e219e82d35b86b79c1aaade4f5a23
Nico Gräf <graef.nico@gmail.com>
2025-04-08 20:34:37 +0000

    feat: add styling and fonts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Website
+
+Local server: `php -S 0.0.0.0:8080`
--- index.php
+++ index.php
@@ -5,46 +5,44 @@ include 'projects.php';
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nico Graef</title>
-    <link rel="stylesheet" href="styles.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nico Graef</title>
+  <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
-    <header>
-        <h1>My Projects</h1>
-    </header>
-    <main>
-        <?php foreach ($projects as $index => $project): ?>
-            <section class="project big <?= $index % 2 ? 'left' : 'right' ?>">
-                <h3>
-                    <?= htmlspecialchars($project->title) ?>
-                    <span style="font-size: small;">
-                        <?php if (!empty($project->linkTitle) && !empty($project->linkUrl)): ?>
-                            <a href="<?= htmlspecialchars($project->linkUrl) ?>"
-                                title="<?= htmlspecialchars($project->linkTitle) ?>">
-                                <?= htmlspecialchars($project->linkTitle) ?>
-                            </a>
-                        <?php endif; ?>
-                    </span>
-                </h3>
-                <div>
-                    <p>
-                        <?php foreach ($project->tags as $tag): ?>
-                            <span class="chip"><?= htmlspecialchars($tag) ?></span>
-                        <?php endforeach; ?>
-                    </p>
-                    <p><?= htmlspecialchars($project->description) ?></p>
-                    <img src="<?= htmlspecialchars($project->image) ?>" alt="<?= htmlspecialchars($project->title) ?>"
-                        title="<?= htmlspecialchars($project->title) ?>" loading="lazy" />
-                </div>
-            </section>
-        <?php endforeach; ?>
-    </main>
-    <footer>
-        <p>&copy; <?= date('Y') ?> nico graef</p>
-    </footer>
+  <main>
+    <?php foreach ($projects as $p): ?>
+      <section class="project">
+        <h3>
+          <span class="title"><?= $p->title ?></span>
+          <?php if (!empty($p->linkTitle) && !empty($p->linkUrl)): ?>
+            <a href="<?= $p->linkUrl ?>" title="<?= $p->linkTitle ?>" target="_blank"
+              rel="noopener noreferrer"><?= $p->linkTitle ?></a>
+          <?php endif; ?>
+        </h3>
+        <div>
+          <p>
+            <?php foreach ($p->tags as $tag): ?>
+              <span class="chip"><?= $tag ?></span>
+            <?php endforeach; ?>
+          </p>
+          <p class="description"><?= $p->description ?></p>
+          <img src="<?= $p->image ?>" alt="<?= $p->title ?>" title="<?= $p->title ?>" loading="lazy" />
+        </div>
+      </section>
+    <?php endforeach; ?>
+  </main>
+  <footer>
+    <p>
+      <a href="https://github.com/nicograef" target="_blank" rel="noopener noreferrer">Github</a>
+      <a href="https://linkedin.com/in/nicograef" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="https://xing.com/profile/Nico_Graef2/" target="_blank" rel="noopener noreferrer">Xing</a>
+    </p>
+    <br />
+    <p>&copy; <?= date('Y') ?> nico graef</p>
+  </footer>
 </body>
 
 </html>
