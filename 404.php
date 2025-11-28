50aed054fda7aca52933a4e2fa2e647c1da2fc06
Nico Gräf <graef.nico@gmail.com>
2025-11-28 20:19:28 +0000

    feat: add 404 page

---
+++ 404.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" type="image/png" href="/img/icon.png">
+    <link rel="stylesheet" href="styles.css">
+    <title>404 - Page Not Found | Nico Gräf</title>
+    <style>
+        .error-container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            min-height: 80vh;
+            text-align: center;
+            padding: 20px;
+        }
+
+        .error-code {
+            font-size: 6rem;
+            font-weight: 700;
+            margin: 0;
+            line-height: 1;
+        }
+
+        .error-message {
+            font-size: 1.5rem;
+            margin: 20px 0;
+        }
+
+        .error-description {
+            margin: 10px 0 30px;
+            color: #666;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="error-container">
+        <img src="/img/404.webp" alt="404 - Page not found" style="max-width: 400px; width: 100%; height: auto; margin-bottom: 20px;">
+        <p>
+            <a href="/">back to my portfolio</a>
+        </p>
+    </div>
+</body>
+
+</html>
