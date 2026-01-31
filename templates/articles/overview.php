da5f8682bb8ea1e7eed78e1983ace177cd53f2e6
Nico Gräf <graef.nico@gmail.com>
2026-01-31 19:14:41 +0000

    fix: refince articles overview design

--- templates/articles/overview.php
+++ templates/articles/overview.php
@@ -8,31 +8,32 @@ ob_start();
 
 <div class="articles-container">
     <a href="/" class="back-link">&larr; Zum Portfolio</a>
+    <h1>Meine Artikel</h1>
     <div class="article-list">
         <?php foreach ($articles as $article): ?>
-            <article class="article-card">
-                <a href="/articles/<?= htmlspecialchars($article['slug']) ?>" class="article-link">
+            <a href="/articles/<?= htmlspecialchars($article['slug']) ?>" class="article-link">
+                <article class="article-card">
                     <h2 class="article-title"><?php if (!empty($article['draft'])): ?>[Entwurf] <?php endif; ?><?= htmlspecialchars($article['title']) ?></h2>
-                </a>
-                <?php if (!empty($article['description'])): ?>
-                    <p class="article-description"><?= htmlspecialchars($article['description']) ?></p>
-                <?php endif; ?>
-                <div class="article-meta">
-                    <?php if ($article['date']): ?>
-                        <span class="date"><?= date('d. F Y', strtotime($article['date'])) ?></span>
+                    <?php if (!empty($article['description'])): ?>
+                        <p class="article-description"><?= htmlspecialchars($article['description']) ?></p>
                     <?php endif; ?>
-                    <?php if ($article['author']): ?>
-                        <span class="author"><?= htmlspecialchars($article['author']) ?></span>
-                    <?php endif; ?>
-                </div>
-                <?php if (!empty($article['tags'])): ?>
-                    <div class="article-tags">
-                        <?php foreach ($article['tags'] as $tag): ?>
-                            <span class="chip"><?= htmlspecialchars($tag) ?></span>
-                        <?php endforeach; ?>
+                    <div class="article-meta">
+                        <?php if ($article['date']): ?>
+                            <span class="date"><?= date('d. F Y', strtotime($article['date'])) ?></span>
+                        <?php endif; ?>
+                        <?php if ($article['author']): ?>
+                            <span class="author"><?= htmlspecialchars($article['author']) ?></span>
+                        <?php endif; ?>
                     </div>
-                <?php endif; ?>
-            </article>
+                    <?php if (!empty($article['tags'])): ?>
+                        <div class="article-tags">
+                            <?php foreach ($article['tags'] as $tag): ?>
+                                <span class="chip"><?= htmlspecialchars($tag) ?></span>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
+                </article>
+            </a>
         <?php endforeach; ?>
     </div>
 </div>
